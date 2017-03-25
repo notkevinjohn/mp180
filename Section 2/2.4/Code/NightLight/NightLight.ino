968a63ce925ea1b61394da59a5ecbeea39952bc4
Kevin John <notkevinjohn@gmail.com>
2017-03-25 15:07:32 -0700

    added some partial docs on breadboards

--- Section 2/2.4/Code/NightLight/NightLight.ino
+++ Section 2/2.4/Code/NightLight/NightLight.ino
@@ -1,30 +1,27 @@
 
-void setup() { 
-  Serial.begin(9600);
-  while(!Serial){}
-  Serial.println("Night Light Starting");
-}
+void setup() { }
 
 void loop() {
-  int light = readLightSensor(); 
-  //Serial.println(light);
+  int light = readLightSensor();   
   if(light>150){
     ledOff();
   }
   else
   {
     ledOn();
-  }
-  //delay(1000);
+  }  
 }
 
 int readLightSensor(){
   return analogRead(5);
 }
+
 int ledPin = 13;
+
 void ledOn(){
   digitalWrite(ledPin, HIGH);
 }
+
 void ledOff(){
   digitalWrite(ledPin, LOW);
 }
