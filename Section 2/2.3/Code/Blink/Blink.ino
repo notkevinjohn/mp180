448bfe31c2795b87366806f151e54f506e744ffc
Kevin John <notkevinjohn@gmail.com>
2017-03-24 21:34:44 -0700

    added LED activity

---
+++ Section 2/2.3/Code/Blink/Blink.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  ledOn();
+  delay(1000);
+  ledOff();
+  delay(1000);
+
+}
+
+int ledPin = 13;
+void ledOn(){
+  digitalWrite(ledPin, HIGH);
+}
+void ledOff(){
+  digitalWrite(ledPin, LOW);
+}
+
