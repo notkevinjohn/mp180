9e2ea3f92d8580fb20afca275d0c1c620a9fef3d
Kevin John <notkevinjohn@gmail.com>
2017-03-28 15:53:14 -0700

    Wrote an instoductory lecture on digital communication

---
+++ Section 3 - SPI Devices/4.1 - SD Card/code/SDCard/SDCard.ino
@@ -0,0 +1,30 @@
+#include <SPI.h>
+#include <SD.h>
+
+const int chipSelect = 10;
+
+void setup() {
+  Serial.begin(9600); 
+  Serial.print("Initializing SD card...");
+ 
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");   
+    while(1);
+  }
+  Serial.println("card initialized.");
+}
+
+void loop() {
+  
+  File dataFile = SD.open("myFile.txt", FILE_WRITE);
+  String dataString = "Hello World";
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();   
+    Serial.println(dataString);
+  } 
+  else {
+    Serial.println("error opening file");
+  }
+  delay(100);
+}
